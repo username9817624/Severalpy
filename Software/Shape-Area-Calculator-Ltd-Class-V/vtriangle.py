6fd1afe26c9965c2623c59ff5eb9fd172e8a2dc7
9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-04 19:10:15 +0200

    vtriangle.py

---
+++ Software/Shape-Area-Calculator-Ltd-Class-V/vtriangle.py
@@ -0,0 +1,9 @@
+class class1:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    @staticmethod
+    def triangle(first, second):
+        variable1 = first * second / 2
+        return variable1

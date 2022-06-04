184d44d17a88a50bb4869ec6e6bf9f8292fef401
9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-04 19:11:10 +0200

    vsquare.py

---
+++ Software/Shape-Area-Calculator-Ltd-Class-V/vsquare.py
@@ -0,0 +1,9 @@
+class class3:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    @staticmethod
+    def square(first, second):
+        variable3 = first * second
+        return variable3

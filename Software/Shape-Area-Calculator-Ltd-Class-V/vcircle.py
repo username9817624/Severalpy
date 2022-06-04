d19c0aff88bc995f7bad2f86b8088fa3db828fad
9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-04 19:10:33 +0200

    vcircle.py

---
+++ Software/Shape-Area-Calculator-Ltd-Class-V/vcircle.py
@@ -0,0 +1,8 @@
+class class2:
+    def __init__(self, first):
+        self.first = first
+
+    @staticmethod
+    def circle(first):
+        variable2 = first * first * 3.14
+        return variable2

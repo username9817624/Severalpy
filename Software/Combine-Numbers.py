55ff5b3041e4a837aee1a7572c0c8174ff9376dd
username9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-07 17:22:46 +0200

    Combine-Numbers.py

---
+++ Software/Combine-Numbers.py
@@ -0,0 +1,6 @@
+while True:
+    print("\n== Combine numbers ==")
+    number = int(input("Enter a number: "))
+    s_number = int(input("Enter a second number: "))
+    final = str(number) + str(s_number)
+    print("Result:", final)

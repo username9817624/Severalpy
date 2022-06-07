a33ca4c8cb8ac626e8a94d264dbef35fe56a85b9
username9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-07 17:32:11 +0200

    Divide-Numbers.py

---
+++ Software/Divide-Numbers.py
@@ -0,0 +1,6 @@
+while True:
+    print("\n== Divide numbers ==")
+    number = float(input("Enter a number: "))
+    s_number = float(input("Enter a second number: "))
+    final = number / s_number
+    print(number, "/", s_number, "=", final)

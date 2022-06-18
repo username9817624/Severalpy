ce5127cba0166f2df0d91375c522672d387d27f9
username9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-18 21:12:12 +0200

    FizzBuzz.py

--- Software/FizzBuzz.py
+++ Software/FizzBuzz.py
@@ -1,6 +1,6 @@
 print("== Fizz Buzz ==")
 for i in range(1, 101):
-    if i % 3 and i % 5 == 0:
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     elif i % 3 == 0:
         print("Fizz")

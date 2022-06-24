2f89a2e95c9b7f92ae32374dd0a3f70a70073b36
username9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-24 13:45:17 +0200

    Find-a-USA-Phone-Number-In-Text.py

--- Software/Find-a-USA-Phone-Number-In-Text.py
+++ Software/Find-a-USA-Phone-Number-In-Text.py
@@ -3,4 +3,4 @@ import re
 while True:
     p_num = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
     u = input("== Find a USA phone number ==\nEnter text: ")
-    print("Numbers found:", p_num.findall(u), "\n")
+    print("Numbers found:", ' '.join(p_num.findall(u)))

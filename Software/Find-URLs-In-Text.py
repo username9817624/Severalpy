e10e74c480ecb12f7a10d67260c6f2f23cefd00c
username9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-24 14:51:58 +0200

    Find-URLs-In-Text.py

--- Software/Find-URLs-In-Text.py
+++ Software/Find-URLs-In-Text.py
@@ -1,6 +1,6 @@
 import re
 
 while True:
-    https = re.compile(r'https?://(?:[a-zA-Z]|\d|[$-_@.&+]|[!*(),]|%[\da-fA-F][\da-fA-F])+')
+    http_s = re.compile(r'https?://(?:[a-zA-Z]|\d|[$-_@.&+]|[!*(),]|%[\da-fA-F][\da-fA-F])+')
     u = input("== Find URLs ==\nEnter text: ")
-    print("URLs found:", ' '.join(https.findall(u)))
+    print("URLs found:", ' '.join(http_s.findall(u)))

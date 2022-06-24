e7bb0b7a20fc2ce0f67be12006d6b751d654ce57
username9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-24 14:38:23 +0200

    Find-Emails-In-Text.py

--- Software/Find-Emails-In-Text.py
+++ Software/Find-Emails-In-Text.py
@@ -1,6 +1,6 @@
 import re
 
 while True:
-    em = re.compile(r'([\w._%+!#$&'*/=?^`{|}~-]+@[\w.-]+\.[a-zA-Z]{2,4})')
+    em = re.compile(r'''([\w._%+!#$&'*/=?^`{|}~-]+@[\w.-]+\.[a-zA-Z]{2,4})''')
     u = input("== Find emails ==\nEnter text: ")
     print("Emails found:", ' '.join(em.findall(u)))

b94573eee7c6253f7a27a34e0de5a122ab01c96d
username9817624 <53998209+username9817624@users.noreply.github.com>
2022-06-30 16:09:55 +0200

    Kitsune-Bakuchi.py

---
+++ Software/Kitsune-Bakuchi.py
@@ -0,0 +1,16 @@
+import random
+import os
+
+while True:
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    d3 = random.randint(1, 6)
+    bet = int(input("== Kitsune bakuchi ==\nEnter a bet: $"))
+    input("Enter anything to roll the dices: ")
+    print("Dices show:", d1, d2, d3)
+    if d1 == d2 == d3:
+        print("Player wins. Payout: $" + str(bet * 4))
+    else:
+        print("Bank wins.")
+    input("\nEnter anything to play again: ")
+    os.system('cls||clear')
